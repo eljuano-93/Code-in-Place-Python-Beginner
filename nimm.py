77d7a3c5f7e29dfc6b7e2d7751463870f1b05444
eljuano-93 <83315816+eljuano-93@users.noreply.github.com>
2022-03-14 15:25:52 -0300

    Update nimm.py

--- nimm.py
+++ nimm.py
@@ -1,16 +1,21 @@
 """
-File: nimm.py
--------------------------
-Add your comments here.
+Nimm is an ancient game of strategy that is named after the old German word for "take."
+It is also called Tiouk Tiouk in West Africa and Tsynshidzi in China. 
+Players alternate taking stones until there are zero left. 
+The game of Nimm goes as follows:
+ 1. The game starts with a pile of 20 stones between the players
+ 2. The two players alternate turns
+ 3. On a given turn, a player may take either 1 or 2 stone from the center pile
+ 4. The two players continue until the center pile has run out of stones.
+The last player to take a stone loses.
 """
+
 NUM_OF_PLAYERS = 2
 
 
 def main():
 
-    """
-    Milestone 3
-    """
+    
     num_of_stones_left = 20
     print("There are "+str(num_of_stones_left)+" stones left")
 
