1956ed2d78b0c6f84ff588be82dbb02234c7d46b
Juan <83315816+eljuano-93@users.noreply.github.com>
2022-03-15 11:49:25 -0300

    Update dog_years.py

--- dog_years.py
+++ dog_years.py
@@ -1,17 +1,27 @@
 """
 dog years
+Everyone knows that our furry friends age at a different rate than humans. Write a program that
+asks the user for a human age (expressed as a whole number) and prints the equivalent dog age
+using the fact that there are seven dog years per human year.
 """
 
 
 def main():
-    total = 0
-    while True:
-        num = int(input("Enter a value: "))
-        if num == 0:
-            break
-        total += num
-        print("Running total is: " + str(total))
+    human_age = int(input("Enter an age in human years: "))
+    print("The age in dog years is " + str(human_age * 7))
 
+
+"""
+Another way to do it defining a function
+
+def main():
+    human_age = int(input("Enter an age in human years: "))
+    print("The age in dog years is " + str(dog_years_age(human_age)))
+
+
+def dog_years_age(n):
+    return n * 7
+"""
 
 # This provided line is required at the end of a Python file
 # to call the main() function.
