3b2d7d897c81f8b077d2ba0859ae31eee81f6671
Juan <83315816+eljuano-93@users.noreply.github.com>
2022-03-15 11:47:36 -0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,27 +1,39 @@
 """
-dog years
-Everyone knows that our furry friends age at a different rate than humans. Write a program that
-asks the user for a human age (expressed as a whole number) and prints the equivalent dog age
-using the fact that there are seven dog years per human year.
+In the game Fizz Buzz, players take turns counting up from one. If a player’s turn lands on a
+number that’s divisible by 3, she should say “fizz” instead of the number, and if it lands on a
+number that’s divisible by 5, she should say “buzz” instead of the number. If the number is both
+a multiple of 3 and of 5, she should say "fizzbuzz" instead of the number.
+It is an interesting problem in control flow and parameter usage. Write a function called fizzbuzz
+which accepts as a parameter an integer called n. The function should count up until and
+including n, fizzing and buzzing the correct numbers along the way. Once it's done, the function
+should return how many numbers were fizzed or buzzed along the way.
+Next, complete your program by writing a main function that reads in an integer from the user
+and plays fizzbuzz until it counts to the number.
+
+Doctest:
+>>> fizz_buzz(3)
+1
+2
+Fizz
 """
 
 
 def main():
-    human_age = int(input("Enter an age in human years: "))
-    print("The age in dog years is " + str(human_age * 7))
+    num_to_count = int(input("Number to count to: "))
+    fizz_buzz(num_to_count)
 
 
-"""
-Another way to do it defining a function
+def fizz_buzz(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 != 0:
+            print("Fizz")
+        if i % 5 == 0 and i % 3 != 0:
+            print("Buzz")
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        if i % 3 != 0 and i % 5 != 0:
+            print(i)
 
-def main():
-    human_age = int(input("Enter an age in human years: "))
-    print("The age in dog years is " + str(dog_years_age(human_age)))
-
-
-def dog_years_age(n):
-    return n * 7
-"""
 
 # This provided line is required at the end of a Python file
 # to call the main() function.
