be0ce87a6c9285dfc0ae6d2e4e1251a43891509e
Juan <83315816+eljuano-93@users.noreply.github.com>
2022-03-15 11:46:56 -0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,30 +1,27 @@
 """
-fizzbuzz
-Doctest:
->>> fizz_buzz(3)
-1
-2
-Fizz
-
+dog years
+Everyone knows that our furry friends age at a different rate than humans. Write a program that
+asks the user for a human age (expressed as a whole number) and prints the equivalent dog age
+using the fact that there are seven dog years per human year.
 """
 
 
 def main():
-    num_to_count = int(input("Number to count to: "))
-    fizz_buzz(num_to_count)
+    human_age = int(input("Enter an age in human years: "))
+    print("The age in dog years is " + str(human_age * 7))
 
 
-def fizz_buzz(n):
-    for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 != 0:
-            print("Fizz")
-        if i % 5 == 0 and i % 3 != 0:
-            print("Buzz")
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
-        if i % 3 != 0 and i % 5 != 0:
-            print(i)
+"""
+Another way to do it defining a function
 
+def main():
+    human_age = int(input("Enter an age in human years: "))
+    print("The age in dog years is " + str(dog_years_age(human_age)))
+
+
+def dog_years_age(n):
+    return n * 7
+"""
 
 # This provided line is required at the end of a Python file
 # to call the main() function.
